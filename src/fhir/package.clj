4f774e313ce681bfd3efbe03571f38475f9f1751
niquola <niquola@gmail.com>
2025-02-13 22:50:23 +0000

    add tests

--- src/fhir/package.clj
+++ src/fhir/package.clj
@@ -2,6 +2,7 @@
   (:require
    [cheshire.core]
    [clojure.string :as str]
+   [clojure.java.io :as io]
    [clojure.java.shell :refer [sh]])
   (:import
    [java.net URL]
@@ -15,12 +16,36 @@
 
 (defn get-registry [] SIMPLIFIER_REPO)
 
-(defn reduce-tar [^String url cb]
-  (let [^URL input-stream-url (URL. url)]
-    (with-open [^InputStream input-stream (.openStream input-stream-url)
+(defn get-tarball-url [pkg-info]
+  (or (get-in pkg-info [:dist :tarball])
+      (when-let [v (get-in pkg-info [:dist-tags :latest])]
+        (get-in pkg-info [:versions (keyword v) :dist :tarball]))))
+
+(defn get-cache-file-name [pkg-info]
+  (let [nm (:name pkg-info)
+        v (or (:version pkg-info) (get-in pkg-info [:dist-tags :latest]))]
+    (str nm "@" v ".tar.gz")))
+
+(defn load-tarball [pkg-info]
+  (let [project-dir (System/getProperty "user.dir")
+        cache-file-name (get-cache-file-name pkg-info)
+        file-path (str project-dir "/.fhir-packages/" cache-file-name)
+        file (io/file file-path)
+        ^URL url (URL. (get-tarball-url pkg-info))]
+    (io/make-parents file-path)
+    ;; cache file
+    (when-not (.exists file)
+      (with-open [^InputStream in (.openStream url)
+                  out (io/output-stream file-path)]
+        (io/copy in out)))
+    file-path))
+
+(defn reduce-tar [pkg-info cb & [acc]]
+  (let [cache-file (load-tarball pkg-info)]
+    (with-open [^InputStream input-stream (io/input-stream cache-file)
                 ^GzipCompressorInputStream gzip-compressor-input-stream (GzipCompressorInputStream. input-stream)
                 ^TarArchiveInputStream tar-archive-input-stream (TarArchiveInputStream. gzip-compressor-input-stream)]
-      (loop [acc {}]
+      (loop [acc (or acc {})]
         (if-let [^TarArchiveEntry entry (.getNextTarEntry tar-archive-input-stream)]
           (let [^String nm (str/replace (.getName entry) #"package/" "")
                 read-fn (fn [& [json]]
@@ -32,56 +57,22 @@
             (recur (cb acc nm read-fn)))
           acc)))))
 
-(defn read-tar [^String url cb]
-  (let [^URL input-stream-url (URL. url)]
-    (with-open [^InputStream input-stream (.openStream input-stream-url)
-                ^GzipCompressorInputStream gzip-compressor-input-stream (GzipCompressorInputStream. input-stream)
-                ^TarArchiveInputStream tar-archive-input-stream (TarArchiveInputStream. gzip-compressor-input-stream)]
-      (loop []
-        (when-let [^TarArchiveEntry entry (.getNextTarEntry tar-archive-input-stream)]
-          (let [^String nm (str/replace (.getName entry) #"package/" "")
-                read-fn (fn [& [json]]
-                          (let [content (byte-array (.getSize entry))]
-                            (.read tar-archive-input-stream content)
-                            (if json
-                              (cheshire.core/parse-string (String. content) keyword)
-                              (String. content))))]
-            (cb nm read-fn))
-          (recur))))))
-
-
-(defn- bash [cmd] (-> (sh "bash" "-c" cmd)))
-
-(defn- from-json [x]
-  (cheshire.core/parse-string x keyword))
-
 (defn pkg-info
   "get package information pkg could be just name a.b.c or versioned name a.b.c@1.0.0"
   [pkg & [registry]]
-  (assert (string? pkg))
-  (let [cmd (str "npm --registry " (or registry (get-registry)) " view --json " pkg)
-        res (bash cmd)
-        out (-> res :out)]
-    (when (or (nil? out) (empty? out))
-      (println :warn res))
-    (try
-      (from-json out)
-      (catch Exception e
-        (throw (Exception. (pr-str res)))))))
-
-(defn pkg-deps
-  [pkg & [registry]]
-  (let [cmd (str "npm --registry " (or registry (get-registry)) " view dependencies --json " pkg)
-        out (-> (bash cmd) :out)]
-    (try
-      (from-json out)
-      (catch Exception e
-        (throw (Exception. out))))))
-
-(defn read-package
-  [pkg on-file]
-  (read-tar (:url pkg) (fn [nm read-resource] (on-file nm read-resource))))
+  (let [[pkg version] (str/split pkg #"@" 2)]
+    (-> (slurp (URL. (str (or registry (get-registry)) "/" pkg)))
+        (cheshire.core/parse-string keyword)
+        (cond-> version
+          (get-in [:versions (keyword version)])))))
 
 (defn reduce-package
-  [pkg on-file]
-  (reduce-tar (:url pkg) (fn [acc nm read-resource] (on-file acc nm read-resource))))
+  "(fn on-file [acc file-name read-resource-fn])"
+  [pkg-info on-file & [acc]]
+  (reduce-tar pkg-info (fn [acc nm read-resource] (on-file acc nm read-resource)) acc))
+
+
+(comment
+
+
+  )
--- test/fhir/package_test.clj
+++ test/fhir/package_test.clj
@@ -2,3 +2,69 @@
   (:require [fhir.package :as subj]
             [matcho.core :as matcho]
             [clojure.test :as t]))
+
+(t/deftest test-fhir-packages
+
+  (matcho/match
+      (subj/pkg-info "hl7.fhir.r4.core")
+    {:name "hl7.fhir.r4.core",
+     :versions
+     {:4.0.1
+      {:name "hl7.fhir.r4.core"
+       :version "4.0.1"}}})
+
+  (t/is (= (subj/get-tarball-url (subj/pkg-info "hl7.fhir.r4.core"))
+           "https://packages.simplifier.net/hl7.fhir.r4.core/4.0.1"))
+
+
+  (def pkg-info (subj/pkg-info "hl7.fhir.r4.core@4.0.1"))
+  (matcho/match
+      pkg-info
+    {:name "hl7.fhir.r4.core",
+     :version "4.0.1",
+     :dist {:tarball string?}})
+
+  (t/is (= (subj/get-tarball-url pkg-info)
+           "https://packages.simplifier.net/hl7.fhir.r4.core/4.0.1"))
+
+  (t/is (thrown?  Exception (subj/pkg-info "hl7.fhir.r4.unexisting")))
+
+  (def bulp-pkg (subj/pkg-info "ihe.iti.balp"))
+
+  (subj/load-tarball bulp-pkg)
+
+  (matcho/match
+      (->> (subj/reduce-package bulp-pkg (fn [acc file-name _read-fn] (conj acc file-name)) [])
+           sort
+           (take 10))
+    [".index.db"
+     ".index.json"
+     "CapabilityStatement-IHE.BALP.ATNA.AuditRecordRepository.json"
+     "CapabilityStatement-IHE.BALP.AuditConsumer.json"
+     "CapabilityStatement-IHE.BALP.AuditCreator.json"
+     "CodeSystem-AuthZsubType.json"
+     "CodeSystem-BasicAuditEntityType.json"
+     "CodeSystem-OtherIdentifierTypes.json"
+     "CodeSystem-UserAgentTypes.json"
+     "ImplementationGuide-ihe.iti.balp.json"])
+
+
+  (def r4-pkg (subj/pkg-info "hl7.fhir.r4.core@4.0.1"))
+
+  (matcho/match
+      (->> (subj/reduce-package r4-pkg (fn [acc file-name _read-fn] (conj acc file-name)) [])
+           sort
+           (take 10))
+    [".index.json"
+     "CapabilityStatement-base.json"
+     "CapabilityStatement-base2.json"
+     "CapabilityStatement-example.json"
+     "CapabilityStatement-knowledge-repository.json"
+     "CapabilityStatement-measure-processor.json"
+     "CapabilityStatement-messagedefinition.json"
+     "CapabilityStatement-phr.json"
+     "CapabilityStatement-terminology-server.json"
+     "CodeSystem-FHIR-version.json"])
+
+
+  )
